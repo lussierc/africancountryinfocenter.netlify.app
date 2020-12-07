e5081fceacb651aab5d8663b277c8d7788a31c3d
Christian Lussier <lussierc@allegheny.edu>
2020-12-07 17:47:54 -0500

    Connect contact page form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,7 +20,7 @@ const ContactPage = ({
           <p>What country do you want to learn more about? Are you ready for your next trip? Do you have any suggestions to improve the website? &rarr;</p>
         </div>
         <div>
-          <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
+          <form className="form-container" action="https://formspree.io/f/mjvpakzo" method="post">
             <div>
               <label htmlFor="w3lName">Name</label>
               <input type="text" name="w3lName" id="w3lName"/>
